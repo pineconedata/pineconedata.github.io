34a13b59a90bfda0049328598e93515b5f0c8c48
Pinecone Data <114192728+pineconedata@users.noreply.github.com>
2024-05-07 21:08:27 -0400

    Fix logical error with container and headings

--- assets/js/table-of-contents.js
+++ assets/js/table-of-contents.js
@@ -1,23 +1,23 @@
-// Generate Table of Contents if toc-list exists
+// Generate Table of Contents if headings exists
 document.addEventListener("DOMContentLoaded", function() {
-  var tocList = document.getElementById('toc-list');
-  
-  // Check tocList before generating TOC
-  if (tocList) {
-    // Create the TOC elements
-    var strongElement = document.createElement('strong');
-    strongElement.textContent = 'Table of Contents';
-    
-    var ulElement = document.createElement('ul');
-    ulElement.id = 'toc-list';
+  var tocContainer = document.getElementById('toc');
+  var headings = document.querySelectorAll('article h2[id], article h3[id]');
+
+  // Check if TOC container and headings exist before generating TOC
+  if (tocContainer && headings.length > 1) {
+    // Create the TOC header
+    var tocTitle = document.createElement('strong');
+    tocTitle.textContent = 'Table of Contents';
+
+    // Create the TOC list
+    var tocList = document.createElement('ul');
+    tocList.id = 'toc-list';
     
     // Append the elements to the TOC container
-    tocContainer.appendChild(strongElement);
-    tocContainer.appendChild(ulElement);
+    tocContainer.appendChild(tocTitle);
+    tocContainer.appendChild(tocList);
     
-    // Find headings and populate the TOC
-    var headings = document.querySelectorAll('article h2[id], article h3[id]');
-    
+    // Populate the TOC
     headings.forEach(function(heading) {
       var listItem = document.createElement('li');
       var link = document.createElement('a');
