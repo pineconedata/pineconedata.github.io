ace7ab8cb6fa9997129641e56543ad1ca1ce09c0
Pinecone Data <114192728+pineconedata@users.noreply.github.com>
2024-05-09 22:28:08 -0400

    Create sources.js

---
+++ assets/js/sources.js
@@ -0,0 +1,43 @@
+// Generate a list of external sources
+window.onload = function(){
+    var sourceContainer = document.getElementById('sources');
+    var currentDomain = window.location.hostname;
+    var sourceLinks = Array.from(document.querySelector('article.blog-post').querySelectorAll('a'));
+    var uniqueLinks = new Set();
+
+    // Iterate through each link and add its href to the Set if it has a different domain
+    sourceLinks.forEach(sourceLink => {
+        if (sourceLink.hostname !== currentDomain) {
+            uniqueLinks.add(sourceLink.href);
+        }
+    });
+    
+    // Convert the Set back to an array
+    var extractedLinks = Array.from(uniqueLinks);
+
+    // Check if the container exists and if there are unique links before generating the list
+    if (sourceContainer && extractedLinks.length > 1) {
+        // Create the source header 
+        var sourceTitle = document.createElement('strong');
+        sourceTitle.content = 'Sources';
+
+        // Create the source list 
+        var sourceList = document.createElement('ul');
+        sourceList.id = 'source-list';
+
+        // Append the elements to the source container
+        sourceContainer.appendChild(sourceTitle);
+        sourceContainer.appendChild(sourceList);
+
+        // Populate the source list 
+        extractedLinks.forEach(function(extractedLink){
+          var listItem = document.createElement('li');
+          var link = document.createElement('a');
+          link.textContent = extractedLink;
+          link.href = extractedLink;
+          link.target = "_blank";
+          listItem.appendChild(link);
+          sourceList.appendChild(listItem);
+        });
+    }
+};
