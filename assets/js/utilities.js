f3e26219b24cc6289119566ad839e62a2f3ee6aa
Pinecone Data <114192728+pineconedata@users.noreply.github.com>
2024-10-31 02:28:07 -0400

    add hidden inputs to mid-post subscription form

--- assets/js/utilities.js
+++ assets/js/utilities.js
@@ -31,13 +31,21 @@ function addMidPostSubscribe() {
     const formHTML = `
             <form action="https://formspree.io/f/xayrvgjj" method="POST" class="form" id="mid-post-subscribe-form" style="padding: 1.25rem; background-color: #f9f9f9; border: 0.0625rem solid #ddd; border-radius: 0.5rem; box-shadow: 0 0.125rem 0.625rem rgba(0, 0, 0, 0.1); margin: 1.25rem 0;">
                 <p>I hope you're enjoying this article! Please subscribe if you'd like to be notified when I publish more content like this.</p>
+                <input type="text" name="_gotcha" style="display:none">
+                <input type="hidden" name="formPageTitle" id="pageTitle">
+                <input type="hidden" name="formPageUrl" id="pageUrl">
+                <input type="hidden" name="_next" value="?message=Thank you for subscribing!">
                 <div style="display: flex; align-items: center;">
                     <input type="email" name="_replyto" class="form-control input-lg" placeholder="Email" title="Email" required="required" style="flex-grow: 1; margin-right: 1em;">
                     <button type="submit" class="btn btn-lg btn-primary">Subscribe</button>
                 </div>
             </form>
         `;
-     midSubscribeContainer.innerHTML += formHTML; 
+    midSubscribeContainer.innerHTML += formHTML; 
+
+    // Set the hidden inputs to the document title and current URL
+    document.getElementById('formPageTitle').value = document.title;
+    document.getElementById('formPageUrl').value = window.location.href;
   }
-}
+};
 addToOnload(addMidPostSubscribe);
