da20a533807fd166b748d30a7caabd7f7dd1b3bb
pineconedata <emily@pineconedata.com>
2024-11-05 01:11:43 -0500

    add h2 elements to table of contents

--- assets/js/table-of-contents.js
+++ assets/js/table-of-contents.js
@@ -1,10 +1,10 @@
-// Generate Table of Contents if headings exists
+// Generate Table of Contents if headings exist
 addToOnload(function() {
   var tocContainer = document.getElementById('toc');
-  var headings = document.querySelectorAll('article h1[id]');
+  var headings = document.querySelectorAll('article h1[id], article h2[id]');
 
   // Check if TOC container and headings exist before generating TOC
-  if (tocContainer && headings.length > 1) {
+  if (tocContainer && headings.length > 0) {
     // Create the TOC header
     var tocTitle = document.createElement('strong');
     tocTitle.textContent = 'Table of Contents';
@@ -17,14 +17,38 @@ addToOnload(function() {
     tocContainer.appendChild(tocTitle);
     tocContainer.appendChild(tocList);
     
-    // Populate the TOC
-    headings.forEach(function(heading) {
+    var currentH1 = null;
+    
+    // Helper function to create a TOC list item with a link
+    function createTocItem(heading) {
       var listItem = document.createElement('li');
       var link = document.createElement('a');
       link.textContent = heading.textContent;
       link.href = '#' + heading.id;
       listItem.appendChild(link);
-      tocList.appendChild(listItem);
+      return listItem;
+    }
+    
+    // Iterate through the headings to create the structure
+    headings.forEach(function(heading) {
+      if (heading.tagName === 'H1') {
+        // For each h1 heading, create a new list item and list
+        var h1ListItem = createTocItem(heading);
+        
+        // Create a new nested list for h2 elements
+        var h2List = document.createElement('ul');
+        
+        // Append the h1 item and the nested list to the main TOC list
+        tocList.appendChild(h1ListItem);
+        h1ListItem.appendChild(h2List);
+        
+        // Set currentH1 to the nested h2List for future h2 headings
+        currentH1 = h2List;
+      } else if (heading.tagName === 'H2' && currentH1) {
+        // For each h2 heading, create a list item inside the current h1's nested list
+        var h2ListItem = createTocItem(heading);
+        currentH1.appendChild(h2ListItem);
+      }
     });
   }
 });
